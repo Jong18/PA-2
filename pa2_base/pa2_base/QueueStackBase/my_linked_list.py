269bbc85c7ca7c8cc873c05072cab3d50e018bbe
Jong18 <89984416+Jong18@users.noreply.github.com>
2023-02-14 11:52:01 +0000

    a

---
+++ pa2_base/pa2_base/QueueStackBase/my_linked_list.py
@@ -0,0 +1,117 @@
+class Node:
+    def __init__(self, data = None, next = None):
+        self.data = data
+        self.next = next
+
+
+class LinkedList():
+    def __init__(self):
+        self.head = None
+        self.tail = None
+    
+    def push_front(self, data):
+        node = Node(data)
+        if self.head == None:
+            self.head = node
+            self.tail = node
+        else:
+            node.data = data
+            node.next = self.head
+        self.head = node
+        
+    
+    def pop_front(self):
+        if self.head == None:
+            return None
+        self.head = self.head.next
+    
+    def push_back(self, data):
+        node = Node(data)
+        if self.head == None:
+            self.head = node
+        else:
+            self.tail.next = node
+        self.tail = node
+
+        # if data != None:   
+        #     new_node = Node(data)
+        #     if self.head == None:
+        #         self.head = new_node
+        #         self.tail = new_node
+        #     else:
+        #         node = self.head
+        #         while node.next != None:
+        #             node = node.next
+        #         node.next = new_node
+        #         self.tail = new_node
+
+
+
+    def pop_back(self):
+        if self.head == None:
+            return None
+        if self.head.next == None:
+            self.head = None
+            self.tail = None
+        else:
+            node = self.head
+            while node.next.next != None:
+                node = node.next
+            node.next = None
+            self.tail = node
+
+        
+
+    def get_size(self):
+        count = 0
+        node = self.head
+        while node != None:
+            count += 1
+            node = node.next
+        return count
+
+
+    def __str__(self):
+        ret_str = ""
+        node = self.head
+        while node != None:
+            ret_str += str(node.data)
+            node = node.next
+            if node != None:
+                ret_str += "\n"
+        return ret_str
+
+
+
+
+
+
+# lis = LinkedList()
+# lis.push_back(3)
+# lis.push_back(1)
+# lis.push_back(6)
+# lis.push_back(9)
+# print("container of size: " + str(lis.get_size()) + ":")
+# # print("\n")
+# print(lis)
+# print(lis.pop_front())
+# print(lis.pop_front())
+print("container of size: " + str(lis.get_size()) + ":")
+# lis = LinkedList()
+# lis.push_back(3)
+# lis.push_back(1)
+# lis.push_back(6)
+# lis.push_back(9)
+# print(lis)
+# new_node = LinkedList()
+# for i in range(1, 6):
+#     new_node.push_front("string " + str(i))
+# print(new_node)
+# new_node.pop_back()
+# print(new_node)
+# new_node.pop_front()
+# print("\n")
+# print(new_node)
+# print(new_node.get_size())
+# new_node.append_node(6)
+# print(new_node)
---
+++ pa2_base/pa2_base/QueueStackBase/my_stack.py
@@ -0,0 +1,24 @@
+from array_deque import ArrayDeque
+from my_linked_list import LinkedList
+
+class Stack:
+    def __init__(self):
+        # Pick one of these to use.
+        # Stack must have the container you dont choose for Queue
+        
+        # self.container = LinkedList()
+        self.container = ArrayDeque()
+
+    def push(self, data):
+        self.container.push_back(data)
+    
+    def pop(self):
+        return self.container.pop_back()
+    
+    def get_size(self):
+        return self.container.get_size()
+
+    def __str__(self):
+        ret_str = ""
+        for i in range(str(self.get_size())):
+            ret_str += str(self.container.container[i] + " ")
